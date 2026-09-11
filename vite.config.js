818b2889c2b36afc13a76e4ca633a2ab0f77a514
micaeltech3 <micaelmbflores@gmail.com>
2026-09-10 21:53:35 -0300

    fix: suporte a navegadores webview android legados

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     basicSsl()
   ],
+  build: {
+    target: ['chrome74', 'es2015']
+  },
   server: {
     https: true,
     host: true
